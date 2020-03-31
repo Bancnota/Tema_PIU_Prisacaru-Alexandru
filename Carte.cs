f8c413c52576d1ed688a1c3c2157a4b2c1077261
Bancnota <62952916+Bancnota@users.noreply.github.com>
2020-03-31 09:29:46 -0700

    Add files via upload

---
+++ Carte.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Tema
+{
+    class Carte
+    {
+        private string nume;
+        private string autor;
+        private int nrpagini;
+        private int pret;
+        public string carti;
+        public int nrCarti;
+
+        public enum gen
+        {
+            Roman = 1,
+            Nuvela = 2,
+            Pastel = 3,
+            Basm = 4
+        }
+        private gen genul = gen.Roman;
+        public gen _gen
+        {
+            get { return genul; }
+            set { genul = value; }
+        }
+        public string NUME
+        {
+            get
+            { return nume; }
+            set
+            { nume = value; }
+        }
+        public string AUTOR
+        {
+            get
+            { return autor; }
+            set
+            { autor = value; }
+        }
+        public int NRPAGINI
+        {
+            get
+            { return nrpagini; }
+            set
+            { nrpagini = value; }
+        }
+        public int PRET
+        {
+            get
+            { return pret; }
+            set
+            { pret = value; }
+        }
+        public Carte()
+        {
+            nume = string.Empty;
+            autor = string.Empty;
+            //  carti = string.Empty;
+            nrpagini = 0;
+            pret = 0;
+            // nrCarti = 0;
+        }
+        public Carte(string Nume, string Autor, int NrPagini, int Pret)
+        {
+            nume = Nume;
+            autor = Autor;
+            //carti = _carti;
+            nrpagini = NrPagini;
+            pret = Pret;
+            // nrCarti = _nrCarti;
+
+        }
+        public Carte(string _nume)
+        {
+            string[] Buff = _nume.Split(',');
+            nume = Buff[0];
+            autor = Buff[1];
+            nrpagini = Convert.ToInt32(Buff[2]);
+            pret = Convert.ToInt32(Buff[3]);
+        }
+        public void NumarareCarti()
+        {
+            for (int i = 0; i < nrCarti; i++)
+            {
+                Console.WriteLine(carti[i]);
+            }
+        }
+       /* public void modificarepret(Carte _pret)
+        {
+            for (int i = 0; i < nrCarti; i++)
+                carti[i].Pret = _pret;
+        }*/
+        public void comparare(Carte db)
+        {
+            if(pret>db.pret)
+            {
+                Console.WriteLine("Cartea {0} scrisa de {1} costa mai mult decat cartea {2} scrisa de {3}", nume, autor, db.nume, db.autor);
+                Console.ReadKey();
+            }
+            if (pret < db.pret)
+            {
+                Console.WriteLine("Cartea {0} scrisa de {1} costa mai mult decat cartea {2} scrisa de {3}", db.nume, db.autor, nume, autor);
+                Console.ReadKey();
+            }
+            if(pret == db.pret)
+            {
+                Console.WriteLine("Au acelasi pret");
+                Console.ReadKey();
+            }
+        }
+        public string Afisare()
+        {
+            return string.Format( "Cartea cu numele <<{0}>>,a fost scrisa de autorul <<{1}>>,are {2} de pagini si costa {3} lei",nume,autor,nrpagini,pret);
+           //Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace Tema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt;
+            int x, y,z;
+            string Nume, Autor, NrPag, Pret;
+            Carte[] carti=new Carte[10];
+            int nrCarti=0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("G.Genul cartii");
+                Console.WriteLine("C.Adaugare carte ");
+                Console.WriteLine("A.Afisare biblioteca ");
+                Console.WriteLine("S.Salvare date in fisier");
+                Console.WriteLine("I.Schimbare date carte");
+                Console.WriteLine("M.Comparare Carti");
+                Console.WriteLine("E.Iesire program");
+                Console.WriteLine("Alegeti o optiune: ");
+                opt = Console.ReadLine();
+            switch (opt)
+                    {
+                    case "G":
+                        Console.WriteLine("Alegeti genul cartii: Roman=1, Nuvela=2,Pastel=3,Basm=4");
+                        carti[0]._gen = (Carte.gen)Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Genul de carte ales este : {0}", carti[0]._gen);
+                        break;
+                    case "C":
+                        Console.WriteLine("Doriti sa introduceti o carte?");
+                        string raspuns = Console.ReadLine();
+                        while (raspuns == "Da" || raspuns=="da"||raspuns=="DA" || raspuns=="dA")
+                        {
+                            Console.WriteLine("Introduceti numele cartii noi: ");
+                             Nume = Console.ReadLine();
+                            Console.WriteLine("Introduceti numele autorului: ");
+                             Autor = Console.ReadLine();
+                            Console.WriteLine("Introduceti numarul de pagini: ");
+                             NrPag = Console.ReadLine();
+                            Int32.TryParse(NrPag, out y);
+                            Console.WriteLine("Introduceti pretul cartii: ");
+                             Pret = Console.ReadLine();
+                            Int32.TryParse(Pret, out x);
+                          Carte s1 = new Carte(Nume, Autor, x, y);
+                            carti[nrCarti] = s1;
+                            nrCarti++;
+                            Console.WriteLine("Doriti sa mai introduceti o carte ?");
+                            string rsp=Console.ReadLine();
+                            if (rsp == "DA" || rsp == "Da" || rsp == "dA" || rsp == "da")
+                            {
+                                continue;
+                            }
+                            else
+                                break;
+                        }break;
+
+                    case "A":
+                        for (int i = 0; i < nrCarti; i++)
+                        {
+                            Console.WriteLine(carti[i].Afisare());
+                            Console.ReadKey();
+                        }
+                        break;
+                    case "S":
+                        using (StreamWriter sr = new StreamWriter("Fisierul meu", true))
+                        {
+                            for (int i = 0; i < nrCarti; i++)
+                            {
+                                
+                                sr.WriteLine(carti[i].Afisare());
+                                
+                            }
+
+                        }
+                        break;
+                    case "M":
+                        if (nrCarti >= 2)
+                        {
+                            carti[0].comparare(carti[1]);
+                        }
+                        else
+                            Console.WriteLine("Pentru comparare trebuiesc cel putin 2 carti");
+                        break;
+                  //  case "E":
+                    //    O(0);
+                   //     break;
+                    
+                        }
+                
+            } while (true);
+               
+           
+        }
+    }
+}
